995f1e616e557cf2890462bb0bd961a5ca4e88df
BlackSwan <blackswan@egret.com>
2016-03-04 10:55:05 +0800

    Class-sample

---
+++ ClassSample/Class_Mesh.ts
@@ -0,0 +1,10 @@
+﻿module egret3d_dev {
+    export class Class_Mesh{
+        constructor() {
+            var mat: MaterialBase = new MaterialBase();
+
+            var geometery: Geometry = new Geometry();
+            var mesh: Mesh = new Mesh(geometery,mat);
+        }
+    }
+}
---
+++ ClassSample/Class_View3D.ts
@@ -0,0 +1,16 @@
+﻿module egret3d_dev {
+    export class Class_View3D {
+        protected _view: View3D;
+        constructor() {
+            this._view = new View3D();
+
+
+
+            this.update();
+        }
+
+        private update() {
+            requestAnimationFrame( ()=>this.update() );
+        }
+    }
+}
