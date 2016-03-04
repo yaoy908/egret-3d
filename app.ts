170d01a89091316659f8aada1450c03bde543a27
Power-cn <wuguojun_0214@live.com>
2016-03-04 12:09:22 +0800

    app.ts

---
+++ app.ts
@@ -0,0 +1,10 @@
+﻿
+function jsOnload() {
+    var view: egret3d_dev.Class_View3D = new egret3d_dev.Class_View3D();
+}
+
+window.onload = () => {
+
+    egret3d_dev.Egret3DEngine.preload(() => jsOnload());
+
+};
