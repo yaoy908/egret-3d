e754c3d84237bb6a36b4ee30cc7693c695b1dbdb
BlackSwan <blackswan@egret.com>
2016-03-07 18:36:39 +0800

    add Color_vertex

---
+++ shader/Color_vertex.glsl
@@ -0,0 +1,14 @@
+﻿attribute vec3 attribute_position ;
+attribute vec3 attribute_normal ;
+attribute vec2 attribute_uv0 ;
+
+uniform mat4 uniform_ModelMatrix ;
+uniform mat4 uniform_ProjectionMatrix ;
+uniform mat4 uniform_normalMatrix ;
+
+varying vec2 varying_uv0  ;
+
+void main(void){
+   highp vec4 temp_p ;
+   temp_p =  uniform_ProjectionMatrix * uniform_ModelMatrix * vec4(attribute_position,1.0) ; 
+}
