e454d88d695d5a8ffca4328dfd3eea05d78cd981
BlackSwan <blackswan@egret.com>
2016-03-11 13:54:51 +0800

    光照渲染

--- ClassSample/Class_DirectLight.ts
+++ ClassSample/Class_DirectLight.ts
@@ -6,9 +6,12 @@
             super();
 
             var mat: ColorMaterial = new ColorMaterial(0xff0000);
-            //mat.drawMode = DrawMode.LINE_STRIP;
             var geometery: CubeGeometry = new CubeGeometry();
             this.cube = new Mesh(geometery, mat);
+
+            var mat2: ColorMaterial = new ColorMaterial(0x0000ff);
+            var geometery2: CubeGeometry = new CubeGeometry();
+            var cube2: Mesh = new Mesh(geometery, mat);
 
             var view1: View3D = new View3D(0, 0, window.innerWidth , window.innerHeight);
             view1.camera3D.lookAt(new Vector3D(0, 0, -1000), new Vector3D(0, 0, 0));
@@ -16,12 +19,16 @@
 
             this._egret3DCanvas.addView3D(view1);
 
+            cube2.x = 80 ;
             view1.addChild3D(this.cube);
+            view1.addChild3D(cube2);
 
             var lights: LightGroup = new LightGroup();
+
             var dirLight: DirectLight = new DirectLight(new Vector3D(-0.5, 0.6, 0.2));
             dirLight.diffuse = 0xff0000ff;
             lights.addDirectLight(dirLight);
+
             this.cube.material.lightGroup = lights;
 
             this._egret3DCanvas.start();
--- ClassSample/Class_Import.ts
+++ ClassSample/Class_Import.ts
@@ -3,11 +3,13 @@
 
         private box: Mesh;
         private view1: View3D;
+
+        private ctl; HoverController;
         constructor() {
             super();
 
             var view1: View3D = new View3D(0, 0, window.innerWidth, window.innerHeight);
-            view1.camera3D.lookAt(new Vector3D(0, 0, -100), new Vector3D(0, 0, 0));
+            view1.camera3D.lookAt(new Vector3D(0, 100, -100), new Vector3D(0, 0, 0));
             view1.backColor = 0xff000000;
 
             this._egret3DCanvas.addView3D(view1);
@@ -19,24 +21,29 @@
             var loa1d: URLLoader = new URLLoader("resource/chahu/Plane001.esm");
             loa1d.onLoadComplete = (e: URLLoader) => this.onLoad(e, "Plane001");
 
+            this.ctl = new HoverController(view1.camera3D);
+
             this._egret3DCanvas.start();
             this._egret3DCanvas.addEventListener(Event3D.ENTER_FRAME, (e) => this.update(e));
         }
 
         protected onLoad(e: URLLoader, name:string) {
-            var mat: ColorMaterial = new ColorMaterial(0xff0000);
+            var mat: ColorMaterial = new ColorMaterial(0xcccccc);
             var mesh: Mesh = new Mesh(e.data, mat);
             this.view1.addChild3D(mesh);
 
             var lights: LightGroup = new LightGroup();
             var dirLight: DirectLight = new DirectLight(new Vector3D(-0.5, 0.6, 0.2));
-            dirLight.diffuse = 0xff0000ff;
+            dirLight.diffuse = 0xffffffff;
+            dirLight.halfColor = 0;
             lights.addDirectLight(dirLight);
             mesh.material.lightGroup = lights;
+
+            this.box = mesh; 
         }
 
         public update(e: Event3D) {
-
+            this.ctl.update();
         }
     }
 }
