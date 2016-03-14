8f19ded645d1d9f5a7789b3fe1aa1aa4c7c58e6d
Power-cn <wuguojun_0214@live.com>
2016-03-14 16:33:53 +0800

    diffuse_vertex.glsl

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 ﻿
 function jsOnload() {
     //var view: egret3d_dev.Class_Mesh = new egret3d_dev.Class_Mesh();
-    var view: egret3d_dev.Class_DirectLight = new egret3d_dev.Class_DirectLight();
+    var view: egret3d_dev.Class_SkinAnimation = new egret3d_dev.Class_SkinAnimation();
 
 }
 
