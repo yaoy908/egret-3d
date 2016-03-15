2308eb3238a26c6188745eefc33419b6da4d26e6
Power-cn <wuguojun_0214@live.com>
2016-03-15 10:58:11 +0800

    diffuse_fragment.glsl

--- shader/end_fs.glsl
+++ shader/end_fs.glsl
@@ -4,6 +4,6 @@ vec4 light ;
 vec3 ambient;
 
 void main() {
-		endColor.xyz = endColor.xyz * light.xyz + specular.xyz * materialSource.specular + ambient.xyz ;
+		endColor.xyz = endColor.xyz * light.xyz + specular.xyz * materialSource.specular + ambient.xyz + diffuse;
         gl_FragColor = endColor ;
 }
