a19018cdcb15c6fbda45b65efb365fe9323ef756
Power-cn <wuguojun_0214@live.com>
2016-03-15 15:46:26 +0800

    Color_vertex.glsl diffuse_vertex.glsl

--- shader/diffuse_vertex.glsl
+++ shader/diffuse_vertex.glsl
@@ -17,32 +17,6 @@ varying vec2 varying_uv0;
 
 vec4 endPosition ;
 
-mat4 buildMat4(int index){
-
-  vec4 quat = uniform_PoseMatrix[index * 2 + 0];
-
-  vec4 translation = uniform_PoseMatrix[index * 2 + 1];
-
-  float xx = quat.x * quat.x;
-  float xy = quat.x * quat.y;
-  float xz = quat.x * quat.z;
-  float xw = quat.x * quat.w;
-
-  float yy = quat.y * quat.y;
-  float yz = quat.y * quat.z;
-  float yw = quat.y * quat.w;
-
-  float zz = quat.z * quat.z;
-  float zw = quat.z * quat.w;
-
-   return mat4(
-	   1.0 - 2.0 * (yy + zz),		2.0 * (xy + zw),		2.0 * (xz - yw),		0,
-	   2.0 * (xy - zw),				1.0 - 2.0 * (xx + zz),	2.0 * (yz + xw),		0,
-	   2.0 * (xz + yw),				2.0 * (yz - xw),		1.0 - 2.0 * (xx + yy),	0,
-	   translation.x,				translation.y,			translation.z,			1
-   );
-}
-
 void main(void){
 
    endPosition = vec4(0.0, 0.0, 0.0, 0.0) ;
