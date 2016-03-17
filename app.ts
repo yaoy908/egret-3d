30251204086e46fcc640c5a67b868c480160f4d8
Power-cn <wuguojun_0214@live.com>
2016-03-17 16:13:56 +0800

    app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 ﻿
 function jsOnload() {
-    //var view: egret3d.Class_Mesh = new egret3d.Class_Mesh();
-    var view: egret3d.Class_MouseEvent = new egret3d.Class_MouseEvent();
+    //var sample: egret3d.Class_Mesh = new egret3d.Class_Mesh();
+    var sample: egret3d.Class_SkinAnimation = new egret3d.Class_SkinAnimation();
 }
 
 window.onload = () => {
