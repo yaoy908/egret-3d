9fcafab38cfd8cef268ba06e8c45f7bb745621fc
BlackSwan <blackswan@egret.com>
2016-03-24 15:42:26 +0800

    添加 多view3D混合示例

---
+++ ClassSample/Class_MuiltView3D.ts
@@ -0,0 +1,26 @@
+﻿module egret3d {
+    export class Class_MuiltView3D {
+
+        protected _egret3DCanvas: Egret3DCanvas;
+
+        private _v1: View3D;
+        private _v2: View3D;
+        private globalTime: number;
+        constructor() {
+            this._egret3DCanvas = new Egret3DCanvas();
+            this._egret3DCanvas.x = 0; 
+            this._egret3DCanvas.y = 0; 
+            this._egret3DCanvas.width = window.innerWidth; 
+            this._egret3DCanvas.height = window.innerHeight; 
+
+            this._v1 = new View3D(0, 0, 500, 500);
+            this._egret3DCanvas.addView3D(this._v1);
+
+            this._v2 = new View3D(500, 0, 500, 500);
+            this._v2.backColor = 0x00ff0000;
+            this._egret3DCanvas.addView3D(this._v2);
+
+            this._egret3DCanvas.start();
+        }
+    }
+}
