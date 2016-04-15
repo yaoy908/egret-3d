3082b1027d92f84ee0c5980a3ea5bf9981cbaccd
BlackSwan <blackswan@egret.com>
2016-04-15 11:29:11 +0800

    添加 地形混合渲染 添加 uvroll 动画 添加 uvSpriteSheet 动画 添加 3种类型雾

---
+++ shader/uvSpriteSheet_fs.glsl
@@ -0,0 +1,7 @@
+uniform float uvSpriteSheet[4] ;
+vec4 diffuseColor ;
+void main() {
+    uv_0.xy *= vec2(uvSpriteSheet[2],uvSpriteSheet[3]);
+    uv_0.xy += vec2(uvSpriteSheet[0],uvSpriteSheet[1]);
+	diffuseColor = texture2D(diffuseTexture , uv_0 );
+}
