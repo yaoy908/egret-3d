2e8ff290ab82038d0d8c4d1f9bdf197c58bbf123
Power-cn <wuguojun_0214@live.com>
2016-04-18 16:18:53 +0800

    控制粒子生存时间

--- shader/diffuse_fragment.glsl
+++ shader/diffuse_fragment.glsl
@@ -1,6 +1,10 @@
 ﻿uniform sampler2D diffuseTexture;
 vec4 diffuseColor ;
 void main() {
+    if( diffuseColor.w == 0.0 ){
+		discard;
+	}
+
 	diffuseColor = texture2D(diffuseTexture , uv_0 );
     
     if( diffuseColor.w == materialSource.cutAlpha ){
