4c19edb2fbd3992818e35c56df86da7200dfcbaa
zwt <379716066@qq.com>
2016-04-18 18:24:21 +0800

    【教程json】 漏传json

--- Sample-wing/src/Main.ts
+++ Sample-wing/src/Main.ts
@@ -3,7 +3,7 @@ class Main extends egret.DisplayObjectContainer {
    
     public constructor() {
         super();
-        var sample = new SampleUvSpriteSheet ();
+        var sample = new SampleUvRoll ();
     }
     
     
