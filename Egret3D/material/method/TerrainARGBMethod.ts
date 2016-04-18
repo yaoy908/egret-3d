2519b8ae61b1f7cc91231c3651d5a69a90d919f9
Power-cn <wuguojun_0214@live.com>
2016-04-18 15:41:44 +0800

    API 的增加

--- Egret3D/material/method/TerrainARGBMethod.ts
+++ Egret3D/material/method/TerrainARGBMethod.ts
@@ -1,9 +1,13 @@
 ﻿module egret3d {
+
     /**
-     * @class egret3d.AOMapMethod
-     * @classdesc
-     * AO贴图方法
-     */
+    * @class egret3d.TerrainARGBMethod
+    * @classdesc
+    * 地形贴图混合渲染方法。
+    * 使用一张贴图中的ARGB色来进行4张贴图进行混合。
+    * @version Egret 3.0
+    * @platform Web,Native
+    */
     export class TerrainARGBMethod extends MethodBase {
 
         private controlTex: ITexture;
@@ -15,9 +19,16 @@
         private uvs: Float32Array = new Float32Array(8);
 
         /**
-         * @language zh_CN
-         * @param texture 
-         */
+        * @language zh_CN
+        * 创建地形贴图混合渲染方法 
+        * @param controlTex 混合贴图
+        * @param splat_0 第一张贴图
+        * @param splat_1 第二张贴图
+        * @param splat_2 第三张贴图
+        * @param splat_3 第四张贴图
+        * @version Egret 3.0
+        * @platform Web,Native
+        */
         constructor(controlTex: ITexture, splat_0: ITexture, splat_1: ITexture, splat_2: ITexture, splat_3: ITexture) {
             super();
             this.methodType = TextureMethodType.diffuse ; 
@@ -40,23 +51,26 @@
         }
 
         /**
-         * @language zh_CN
-         * 设置 UVTitling。
-         * @param index {Number} 图层索引
-         * @param x {Number} u 的重复次数
-         * @param y {Number} v 的重复次数
-         * @version Egret 3.0
-         * @platform Web,Native
-         */
+        * @language zh_CN
+        * 设置 UVTitling。
+        * @param index {Number} 图层索引
+        * @param x {Number} u 的重复次数
+        * @param y {Number} v 的重复次数
+        * @version Egret 3.0
+        * @platform Web,Native
+        */
         public setUVTitling(index: number, x: number, y: number) {
             this.uvs[index * 2] = x;
             this.uvs[index * 2+1] = y;
         }
 
         /**
-         * @language zh_CN
-         * @param texture 
-         */
+        * @language zh_CN
+        * 设置第一张贴图
+        * @param texture 贴图
+        * @version Egret 3.0
+        * @platform Web,Native
+        */
         public set splat_0_Texture(texture: ITexture) {
             this.splat_0 = texture;
             this.materialData.splat_0Tex = texture; 
@@ -64,9 +78,12 @@
         }
 
         /**
-         * @language zh_CN
-         * @param texture 
-         */
+        * @language zh_CN
+        * 设置第二张贴图
+        * @param texture 贴图
+        * @version Egret 3.0
+        * @platform Web,Native
+        */
         public set splat_1_Texture(texture: ITexture) {
             this.splat_1 = texture;
             this.materialData.splat_1Tex = texture;
@@ -75,9 +92,12 @@
         }
 
         /**
-         * @language zh_CN
-         * @param texture 
-         */
+        * @language zh_CN
+        * 设置第三张贴图
+        * @version Egret 3.0
+        * @platform Web,Native
+        * @param texture 贴图
+        */
         public set splat_2_Texture(texture: ITexture) {
             this.splat_2 = texture;
             this.materialData.splat_2Tex = texture;
@@ -85,9 +105,12 @@
         }
 
         /**
-         * @language zh_CN
-         * @param texture 
-         */
+        * @language zh_CN
+        * 设置第四张贴图
+        * @param texture 贴图
+        * @version Egret 3.0
+        * @platform Web,Native
+        */
         public set splat_3_Texture(texture: ITexture) {
             this.splat_3 = texture;
             this.materialData.splat_3Tex = texture;
@@ -96,9 +119,12 @@
 
 
         /**
-         * @language zh_CN
-         * @param texture 
-         */
+        * @language zh_CN
+        * 设置混合贴图
+        * @param texture 贴图
+        * @version Egret 3.0
+        * @platform Web,Native
+        */
         public set controlTexture(texture: ITexture) {
             this.controlTex = texture;
             this.materialData.blendMaskTexture = texture;
@@ -106,6 +132,7 @@
         }
 
         /**
+        * @private
         * @language zh_CN
         * @param time
         * @param delay
@@ -120,14 +147,19 @@
         public upload(time: number, delay: number, usage: PassUsage, geometry: SubGeometry, context3DProxy: Context3DProxy, modeltransform: Matrix4_4, camera3D: Camera3D) {
             usage["uvs"] = context3DProxy.getUniformLocation(usage.program3D, "uvs"); 
         }
-
+        
+        /**
+        * @language zh_CN
+        * @private
+        */
         public update(time: number, delay: number, usage: PassUsage, geometry: SubGeometry, context3DProxy: Context3DProxy, modeltransform: Matrix4_4, camera3D: Camera3D) {
             context3DProxy.uniform1fv(usage["uvs"], this.uvs);
         }
 
         /**
-         * @language zh_CN
-         */
+        * @language zh_CN
+        * @private
+        */
         public dispose() {
         }
     }
