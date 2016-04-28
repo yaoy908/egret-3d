eaa2c81c88ce1ff210c1b7f64a27c470203f7f5c
Power-cn <wuguojun_0214@live.com>
2016-04-28 11:02:09 +0800

    增加onTsconfig

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
 ﻿
 function jsOnload() {
-    var sample: egret3d.Class_QuadTree = new egret3d.Class_QuadTree();
+    var sample: egret3d.Class_Wireframe = new egret3d.Class_Wireframe();
+}
+
+function onTsconfig() {
+    egret3d.Egret3DEngine.addImportScript("./js/ClassSample/Class_Wireframe.js");
 }
 
 window.onload = () => {
+    egret3d.Egret3DEngine.onTsconfig = () => onTsconfig();
 
     egret3d.Egret3DEngine.preload(() => jsOnload());
 }
