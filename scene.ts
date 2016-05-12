b419fb3cd7a36d457adc99d4f2d12db27af205bc
hellmor <dymmky@126.com>
2016-05-12 18:57:01 +0800

    加入播放音乐的代码

--- scene.ts
+++ scene.ts
@@ -1,9 +1,35 @@
 ﻿//场景目录名
 let SceneName: string = "Sponza";
 
-function startScene() {
+var audioManager: egret3d.AudioManager;
+var bgSound: egret3d.Sound;
 
+window.addEventListener("click", egretMouseClickAndPlay);
+window.addEventListener("touchstart", egretMouseClickAndPlay);
+function startScene() {
     egret3d.EgretWorld.getInstance().loadScene(SceneName);
+    playSound();
+}
+
+function egretMouseClickAndPlay(e) {
+    window.removeEventListener("click", egretMouseClickAndPlay);
+    window.removeEventListener("touchstart", egretMouseClickAndPlay);
+    playSound();
+}
+
+function playSound() {
+    audioManager = egret3d.AudioManager.instance;
+    if (audioManager && audioManager.hasAudio()) {
+        bgSound = audioManager.createSound("./resource/music/bg.mp3", onSoundComplete);
+    }
+    function onSoundComplete(...args) {
+        var options: any = { "volume": 0.8, "loop": true };
+        var result: any = audioManager.playSound(bgSound, options);
+        if (result) {
+            window.removeEventListener("click", egretMouseClickAndPlay);
+            window.removeEventListener("touchstart", egretMouseClickAndPlay);
+        }
+    }
 }
 
 function onMyTsconfig() {
@@ -18,7 +44,6 @@ function onMyTsconfig() {
 }
 
 window.onload = () => {
-
     egret3d.Egret3DEngine.onTsconfig = () => onMyTsconfig();
     egret3d.Egret3DEngine.preload(() => startScene());
 }
