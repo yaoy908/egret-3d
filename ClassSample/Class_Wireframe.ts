adc74406d2991cff750e0ce49dd69435f9d5e5a3
Power-cn <wuguojun@egret.com>
2016-05-12 10:36:16 +0800

    修改画线算法

--- ClassSample/Class_Wireframe.ts
+++ ClassSample/Class_Wireframe.ts
@@ -19,41 +19,136 @@
             this.cameraCtl.rotationX = 60;
 
             var wir: egret3d.Wireframe = new egret3d.Wireframe();
-            wir.material.diffuseColor = 0xffff00;
+            wir.material.diffuseColor = 0xffffff;
+            wir.material.ambientColor = 0xffffff;
             this.view1.addChild3D(wir);
 
             var geom: egret3d.Geometry = wir.geometry;
 
-            var width: number = 20;
-            var height: number = 20;
+            var width: number = 200;
+            var height: number = 200;
 
-            var row: number = 200;
-            var col: number = 200;
+            var row: number = 10;
+            var col: number = 10;
 
-            for (var i: number = 0; i < row; i++) {
-                for (var j: number = 0; j < col; j++) {
-                    var index: number = i * col + j;
-                    geom.setVerticesForIndex(index, egret3d.VertexFormat.VF_POSITION, [width * j - width * col / 2, 0, height * i - height * row / 2]);
+            var point_row: number = row + 1;
+            var point_col: number = col + 1;
 
-                    if (j + 1 < col) {
-                        geom.indexData.push(i * col + j);
-                        geom.indexData.push(i * col + j + 1);
-                    }
+            var vb: Array<number> = new Array<number>();
+            var ib: Array<number> = new Array<number>();
 
-                    if (i + 1 < row) {
-                        geom.indexData.push(i * col + j);
-                        geom.indexData.push((i + 1) * col + j);
-                    }
-                }
+            for (var i: number = 0; i < point_row; ++i) {
+                vb.push(-width * col / 2, 0, height * i - height * row / 2);
+                vb.push(width * col / 2, 0, height * i - height * row / 2);
             }
 
-            this._egret3DCanvas.start();
+            for (var i: number = 0; i < point_col; ++i) {
+                vb.push(width * i - width * col / 2, 0, height * col / 2);
+                vb.push(width * i - width * col / 2, 0, -height * col / 2);
+            }
+
+            for (var i: number = 0; i < vb.length; ++i) {
+                ib.push(i);
+            }
+
+            geom.setVerticesForIndex(0, egret3d.VertexFormat.VF_POSITION, vb, vb.length / 3);
+            geom.setVertexIndices(0, ib);
+
+            var box: Mesh = new Mesh(new CubeGeometry(), new TextureMaterial());
+            this.view1.addChild3D(box);
+
+            //var m: Mesh = new Mesh(new PlaneGeometry(), new TextureMaterial());
+            //this.view1.addChild3D(m);
             this._egret3DCanvas.addEventListener(Event3D.ENTER_FRAME, this.update, this);
+
+            //var load: URLLoader = new URLLoader("resource/laohu/body_12.esm");
+            //load.addEventListener(LoaderEvent3D.LOADER_COMPLETE, this.onLoad, this);
+
+
+            //this._line[0] = this.buildSemicircleLien(egret3d.Vector3D.X_AXIS, 80, 25, 180, 180, 0xffff00);
+            //this.view1.addChild3D(this._line[0]);
+            //this._line[1] = this.buildSemicircleLien(egret3d.Vector3D.Y_AXIS, 80, 25, 180, 180, 0xffffffff);
+            //this.view1.addChild3D(this._line[1]);
+            //this._line[2] = this.buildSemicircleLien(egret3d.Vector3D.Z_AXIS, 80, 25, 180, 270, 0xffffffff);
+            //this.view1.addChild3D(this._line[2]);
+
+
         }
+
+        private _line: Array<egret3d.Wireframe> = new Array<egret3d.Wireframe>();
     
+        protected onLoad(e: LoaderEvent3D) {
+            var m: Mesh = new Mesh(e.loader.data, new TextureMaterial());
+            this.view1.addChild3D(m);
+        }
 
         public update(e: Event3D) {
             this.cameraCtl.update();
+        }
+
+        private buildSemicircleLien(axis: egret3d.Vector3D, r: number, density: number, angle: number, offsetAngle: number, color: number): egret3d.Wireframe {
+            var line: egret3d.Wireframe = null;
+            var vb = [];
+            if (axis.x != 0) {
+                for (var i = 0; i < density; i++) {
+                    vb.push(
+                        0,
+                        r * Math.cos(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * i)),
+                        r * Math.sin(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * i))
+                    );
+                }
+
+                vb.push(
+                    0,
+                    r * Math.cos(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * (density - 1))),
+                    r * Math.sin(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * (density - 1)))
+                );
+            }
+            else if (axis.y != 0) {
+                for (var i = 0; i < density; i++) {
+                    vb.push(
+                        r * Math.cos(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * i)),
+                        0,
+                        r * Math.sin(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * i))
+                    );
+                }
+
+                vb.push(
+                    r * Math.cos(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * (density - 1))),
+                    0,
+                    r * Math.sin(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * (density - 1)))
+                );
+            }
+            else if (axis.z != 0) {
+                for (var i = 0; i < density; i++) {
+                    vb.push(
+                        r * Math.cos(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * i)),
+                        r * Math.sin(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * i)),
+                        0);
+                }
+
+                vb.push(
+                    r * Math.cos(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * (density - 1))),
+                    r * Math.sin(egret3d.MathUtil.DEGREES_TO_RADIANS * (offsetAngle + angle / density * (density - 1))),
+                    0);
+            }
+            else return null;
+
+            var ib = [];
+            for (var i = 1; i <= density; i++) {
+                ib.push(i - 1, i);
+            }
+
+            // var geom: egret3d.Geometry = new egret3d.Geometry();
+            // geom.vertexFormat = egret3d.VertexFormat.VF_POSITION | egret3d.VertexFormat.VF_COLOR;
+            // geom.verticesData = vb;
+            // geom.indexData = ib;
+            line = new egret3d.Wireframe();
+            var geom: egret3d.Geometry = line.geometry;
+            geom.setVerticesForIndex(0, egret3d.VertexFormat.VF_POSITION, vb, vb.length / 3);
+            geom.indexData = ib;
+            line.material.diffuseColor = color;
+            return line;
         }
     }
 }
