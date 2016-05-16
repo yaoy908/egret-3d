a70e4c932c84b518b03ad25f47f5fbe60dc1c15c
Power-cn <wuguojun@egret.com>
2016-05-16 16:34:52 +0800

    index.html

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 ﻿
 function jsOnload() {
-    var sample: egret3d.Class_SkinAnimation = new egret3d.Class_SkinAnimation();
+    var sample: egret3d.Class_Sky = new egret3d.Class_Sky();
 }
 
 function onTsconfig() {
+    egret3d.Egret3DEngine.addImportScript("./js/ClassSample/Class_View3D.js");
+    egret3d.Egret3DEngine.addImportScript("./js/ClassSample/Class_Wireframe.js");
     egret3d.Egret3DEngine.addImportScript("./js/ClassSample/Class_SkinAnimation.js");
+    egret3d.Egret3DEngine.addImportScript("./js/ClassSample/Class_Sky.js");
+    egret3d.Egret3DEngine.addImportScript("./js/ClassSample/Class_Particle.js");
 }
 
 window.onload = () => {
