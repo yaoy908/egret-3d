1553882e2316b4469e371d4180aaa050004587e5
hellmor <dymmky@126.com>
2016-07-20 11:14:00 +0800

    加入支持粒子的speedScale功能

--- shader/particle_end_fs.glsl
+++ shader/particle_end_fs.glsl
@@ -7,10 +7,10 @@ void main() {
 	outColor.xyz = diffuseColor.xyz * materialSource.diffuse * varying_color.xyz ;
 	outColor.w = materialSource.alpha * diffuseColor.w * varying_color.w;
 	outColor.xyz *= outColor.w;
-	if(blendMode == 0.0 || blendMode == 2.0 || blendMode == 8.0){
-	}else{
-		outColor.xyz *= 1.8;
-	}
+	// Add Mode
+	//if(blendMode == 4.0){
+	//	outColor.xyz *= 1.8;
+	//}
     gl_FragColor = outColor;
 }
 
